55129496e47cb8cc7507c5aa0ab6c49a89ef438a
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-03-05 16:35:05 +0800

    :children_crossing: Improve shell completions

--- src/requirements_language_server/__main__.py
+++ src/requirements_language_server/__main__.py
@@ -3,11 +3,15 @@ r"""This module can be called by
 """
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-from contextlib import suppress
 from datetime import datetime
 
 from . import __name__ as NAME
 from . import __version__
+
+try:
+    import shtab
+except ImportError:
+    import _shtab as shtab
 
 NAME = NAME.replace("_", "-")
 VERSION = rf"""{NAME} {__version__}
@@ -25,36 +29,32 @@ def get_parser() -> ArgumentParser:
         epilog=EPILOG,
         formatter_class=RawDescriptionHelpFormatter,
     )
-    with suppress(ImportError):
-        import shtab
-
-        shtab.add_argument_to(parser)
+    shtab.add_argument_to(parser)
     parser.add_argument("--version", version=VERSION, action="version")
-    parser.add_argument(
-        "--check",
-        nargs="*",
-        default=[],
-        help="check file's errors and warnings",
-    )
-    parser.add_argument(
-        "--format",
-        nargs="*",
-        default=[],
-        help="format files",
-    )
     parser.add_argument(
         "--color",
         choices=["auto", "always", "never"],
         default="auto",
         help="when to display color",
     )
+    parser.add_argument(
+        "--check",
+        nargs="*",
+        default={},
+        help="check file's errors and warnings",
+    ).complete = shtab.FILE  # type: ignore
+    parser.add_argument(
+        "--format",
+        nargs="*",
+        default={},
+        help="format files",
+    ).complete = shtab.FILE  # type: ignore
     return parser
 
 
 def main() -> None:
     r"""Parse arguments and provide shell completions."""
-    parser = get_parser()
-    args = parser.parse_args()
+    args = get_parser().parse_args()
 
     from lsp_tree_sitter.diagnose import check
     from lsp_tree_sitter.format import format
@@ -63,16 +63,17 @@ def main() -> None:
     from .finders import DIAGNOSTICS_FINDER_CLASSES, FORMATTING_FINDER_CLASSES
     from .utils import get_filetype
 
-    format(args.format, parse, FORMATTING_FINDER_CLASSES, get_filetype)  # type: ignore
-    result = check(
-        args.check,
-        parse,
-        DIAGNOSTICS_FINDER_CLASSES,
-        get_filetype,
-        args.color,
-    )
     if args.format or args.check:
-        exit(result)
+        format(args.format, parse, FORMATTING_FINDER_CLASSES, get_filetype)  # type: ignore
+        exit(
+            check(
+                args.check,
+                parse,
+                DIAGNOSTICS_FINDER_CLASSES,
+                get_filetype,
+                args.color,
+            )
+        )
 
     from .server import RequirementsLanguageServer
 
